1b1744e53f2463ec3b2cb4f22209b10f127b13c2
Anup173 <anupdas17121997@gmail.com>
2020-05-16 16:12:48 +0530

    Update even_sum.py

--- even_sum.py
+++ even_sum.py
@@ -5,20 +5,20 @@ def even_sum(n1,n2,n3,n4,n5):
          
       else:
          even=even+0;
-      if(n2%2!=0):
+      if(n2%2==0):
          even=even+n2;
       else:
          even=even+0;
     
-      if(n3%2!=0):
+      if(n3%2==0):
          even=even+n3;
       else:
          even=even+0;
-      if(n4%2!=0):
+      if(n4%2==0):
          even=even+n4;
       else:
          even=even+0;
-      if(n5%2!=0):
+      if(n5%2==0):
          even=even+n5;
       else:
          even=even+0;
