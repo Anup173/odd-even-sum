331ea70a24b75e88f336a462454f471cde1b84c5
Anup173 <anupdas17121997@gmail.com>
2020-05-16 14:48:47 +0530

    Add files via upload

---
+++ odd_sum.py
@@ -0,0 +1,28 @@
+def odd_sum(n1,n2,n3,n4,n5): 
+      odd = 0
+      if(n1%2!=0):
+       
+         odd=odd+n1;
+         
+      else:
+         odd=odd+0;
+      if(n2%2!=0):
+         odd=odd+n2;
+      else:
+         odd=odd+0;
+    
+      if(n3%2!=0):
+         odd=odd+n3;
+      else:
+         odd=odd+0;
+      if(n4%2!=0):
+         odd=odd+n4;
+      else:
+         odd=odd+0;
+      if(n5%2!=0):
+         odd=odd+n5;
+      else:
+         odd=odd+0;
+      return odd 
+
+   
