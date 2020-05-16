8f8d045444983809d7aa29c2fface115dfe19ea2
Anup173 <anupdas17121997@gmail.com>
2020-05-16 14:51:54 +0530

    Add files via upload

---
+++ even_sum.py
@@ -0,0 +1,27 @@
+def even_sum(n1,n2,n3,n4,n5): 
+      even = 0
+      if(n1%2==0):
+         even =even+n1;
+         
+      else:
+         even=even+0;
+      if(n2%2!=0):
+         even=even+n2;
+      else:
+         even=even+0;
+    
+      if(n3%2!=0):
+         even=even+n3;
+      else:
+         even=even+0;
+      if(n4%2!=0):
+         even=even+n4;
+      else:
+         even=even+0;
+      if(n5%2!=0):
+         even=even+n5;
+      else:
+         even=even+0;
+      return even 
+
+   
